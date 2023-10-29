c9c3b0b06a41f33e6a20f8a8620662b42c0bb109
ira089 <gvir2015@gmail.com>
2023-10-29 17:34:57 +0200

    js07_fin1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ const galleryUl = document.querySelector(".gallery");
 const markupImg = galleryItems
   .map(
     ({ preview, original, description }) =>
-      `<li class="gallery__item"><a class="gallery__link" target="_self" href="${preview}"> <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" /></a></li>`
+      `<li class="gallery__item"><a class="gallery__link"  href="${preview}"> <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" /></a></li>`
   )
   .join(" ");
 
@@ -17,7 +17,7 @@ galleryUl.addEventListener("click", onClick);
 function onClick(evt) {
   //   console.log(evt.target);
   evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
+  if (!evt.target.classList.contains("gallery__image")) {
     return;
   }
   const imgOriginal = evt.target.dataset.source;
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+const markupImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link"  href="${original}"> <img class="gallery__image" src="${original}" alt ="${description}"/></a></li>`
+  )
+  .join(" ");
+
+galleryUl.innerHTML = markupImg;
+// console.dir(galleryUl);
+
+const settings = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+// console.dir(settings);
+
+let gallery = new SimpleLightbox(".gallery__link", settings);
